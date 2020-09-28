d03603328dea5ab2b79473f99fb95476dd8b6a40
hankOlofs <treffin@gmail.com>
2020-09-28 16:16:40 +0200

    Tests with the API.

--- API fetch.R
+++ API fetch.R
@@ -10,9 +10,28 @@ library(XML)
 # install.packages("geojsonio")
 library(geojsonio)
 
+#install.packages("geojsonR")
+library(geojsonR)
+
+# install.packages("RCurl")
+library(RCurl)
+
+library(sp)
+
+#https://ropensci.org/blog/2013/10/23/style-geojson-polygon/
 
 date <- "1990-09-28"
-url<-paste0("http://api.thenmap.net/v2/se-4/geo/", date)
+url <- paste0("http://api.thenmap.net/v2/world-2/geo/", date)
+download.file(url = url, "test.geojson", replace = TRUE)
 
-map_data <- geojson_json(url, what = "list")
-map_data <- geojson_read()
+# file_fetch <- getURL(url)
+# as_json <- toJSON(file_fetch)
+# 
+# map_data2 <- geojson_list(as_json)
+# map_data <- fromJSON(url)
+
+# map_data3 <- geojson_json(map_data, type = "FeatureCollection")
+
+map_data <- geojson_read("test.geojson", what = "sp")
+
+plot(map_data)
