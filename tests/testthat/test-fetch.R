88c09ad461d78db3b867c101fdbb8c45764fb739
sofiejorgensen <sofficial.jorgensen@gmail.com>
2020-10-02 16:12:22 +0200

    added some tests

--- tests/testthat/test-fetch.R
+++ tests/testthat/test-fetch.R
@@ -5,3 +5,13 @@
 # test_that("input string is correct", {
 #   is.character(fetch(date_str))
 # })
+
+test_that("Wrong input throws an error.", {
+  expect_error(fetch("2000-02-99"))
+  expect_error(fetch("01-01-2000"))
+  expect_error(fetch("2005-2020"))
+  expect_error(fetch("20000110"))
+  expect_error(fetch("0000-00-00"))
+  expect_error(fetch("Hello"))
+  expect_error(fetch(2000-01-01))
+})
