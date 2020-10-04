ddf4b605efe6f029919cd6cb1bb7a6b59ed24265
hankOlofs <treffin@gmail.com>
2020-10-04 12:38:25 +0200

    Several extensions to the fetch function to allow for fetching from different url's.

--- R/fetch.R
+++ R/fetch.R
@@ -2,8 +2,9 @@
 #' 
 #' @description API fetch function
 #'
-#' @param date_str a date string expressed as "yyyy-mm-dd"
-#' @param ... optional parameters
+#' @param date_str a date string expressed as "yyyy-mm-dd". if contains_date is set to FALSE, this should be left blank
+#' @param url a url pointing to a geojson file
+#' @param contains_date a boolean indicating whether or not to include the date_str at the end of the url
 #'
 #' @return Returns a list with the data as well as a note if there was no data the specified date.
 #' @export 
@@ -12,22 +13,36 @@
 #'
 #' @examples
 #' fetch("2000-01-01")
-fetch <-
-function(date_str = "2000-01-01", ...) {
-  date <- as.Date(date_str)
-  notes <- "" # pass a note if the date is missing
+#' 
+# if no date is specified, use today's date
+fetch <- function(date_str = as.character(Sys.Date()), url = "http://api.thenmap.net/v2/world-2/geo/", contains_date = TRUE) {
+  stopifnot("Error, date_str must be a character string on the form yyyy-MM-dd." = is.character(date_str))
+  stopifnot("Error, url must be a character string." = is.character(url))
+  stopifnot("Error, contains_date must be boolean." = is.logical(contains_date))
   
-  # function to fetch data
-  fetch_data <- function(date) {
-    url <- paste0("http://api.thenmap.net/v2/world-2/geo/", date)
+  date <- as.Date(date_str)
+  notes <- "" # pass a note if the date is missing for some reason
+  
+  # constructs a url to fetch data from 
+  urler <- function(url, date, contains_date) {
+    if (contains_date == TRUE) {
+      url <- paste0(url, date)
+    }
+    return(url)
+  }
+  
+  url <- urler(url = url, date = date, contains_date = contains_date)
+  
+  # function to fetch data and convert to a simple features object
+  fetch_data <- function(url) {
     fetched_data <- geojsonio::geojson_sf(url)
     return(fetched_data)
   }
   
   # actually fetching the data
-  fetched_data <- fetch_data(date)
+  fetched_data <- fetch_data(url)
   
-  # a nearly empty string should trigger a search for the next available date
+  # a nearly empty object should trigger a search for the next available date
   cond <- function(fd) {
     utils::object.size(fd) < 100
   }
@@ -36,13 +51,19 @@ function(date_str = "2000-01-01", ...) {
     while (cond(fetched_data)) {
       date <-  date + 1
       fetched_data <- fetch_data(date)
-      # add something to make the loop always end
+      if(date - as.Date(date_str) > 100) {
+        no_data <- 1
+        break
+      }
     }
-    notes <-
-      paste(
+    if (no_data == 1) {
+      notes <- "Error, there does not seem to be any data to fetch."
+    } else {
+      notes <- paste(
         "The data was not available for the selected date. Data fetched for next available date instead:",
         date
       )
+    }
   }
   
   if (notes == "") {
@@ -52,5 +73,5 @@ function(date_str = "2000-01-01", ...) {
   # returning a list with the current geojson file and a note
   # might make map_geojson a list of every fetched piece of data to preserve and speed up old queries
   # (although that will start using too much space - perhaps the last 20 or something)
-  return(list("data" = fetched_data, "notes" = notes))
+  return(list("data" = fetched_data, "notes" = paste0("Data fetched from: ", url, ". ", notes)))
 }
