8f829a89dafa467501fa9d6634b1efc561dd42f7
hankOlofs <treffin@gmail.com>
2020-10-04 17:59:21 +0200

    Testing

--- R/fetch.R
+++ R/fetch.R
@@ -21,8 +21,8 @@ fetch <- function(date_str = as.character(Sys.Date()), url = "http://api.thenmap
   # block of input checks
   stopifnot("Error, date_str must be a character string on the form yyyy-MM-dd or blank." = is.character(date_str))
   stopifnot("Error, date_str must be a character string on the form yyyy-MM-dd or blank." = nchar(date_str) == 10)
-  stopifnot("Error, check format and contents of date_str (yyyy-MM-dd)." = class(try(as.Date(date_str), silent = TRUE)) == "Date")
-  stopifnot("Error, check format and contents of date_str (yyyy-MM-dd)." = grepl("....-..-..", date_str, fixed = FALSE))
+  # stopifnot("Error, check format and contents of date_str (yyyy-MM-dd)." = class(try(as.Date(date_str), silent = TRUE)) == "Date")
+  # stopifnot("Error, check format and contents of date_str (yyyy-MM-dd)." = grepl("....-..-..", date_str, fixed = FALSE))
   stopifnot("Error, URL must be a character string." = is.character(url))
   stopifnot("The URL provided does not have the correct format" = grepl(".*tp.?://.*\\..*/.*", url, fixed = FALSE))
   stopifnot("Error, contains_date must be boolean." = is.logical(contains_date))
