aa3413f856c5fc9f482561aebf24987c07dba3b1
hankOlofs <treffin@gmail.com>
2020-10-04 13:37:26 +0200

    Several updates to the functionality and input checks.

--- R/fetch.R
+++ R/fetch.R
@@ -14,24 +14,50 @@
 #' @examples
 #' fetch("2000-01-01")
 #' 
-# if no date is specified, use today's date
+# fetch inputs are date, url and a boolean to exclude date if fetching from a static source (i.e. no date parameter)
+# if no date is specified, today's date is used
 fetch <- function(date_str = as.character(Sys.Date()), url = "http://api.thenmap.net/v2/world-2/geo/", contains_date = TRUE) {
+  
+  # block of input checks
   stopifnot("Error, date_str must be a character string on the form yyyy-MM-dd or blank." = is.character(date_str))
   stopifnot("Error, date_str must be a character string on the form yyyy-MM-dd or blank." = nchar(date_str) == 10 )
   stopifnot("Error, url must be a character string." = is.character(url))
   stopifnot("Error, contains_date must be boolean." = is.logical(contains_date))
+  stopifnot("Error, date_str input seems to be misspelled" = class(try(as.Date(date_str), silent = TRUE)) == "Date")
   
+  # checking if user really wants to use a different API than intended
+  if (!grepl("api.thenmap.net", url, fixed = TRUE)) {
+    print(
+      "You seem to have chosen a non-standard url. This API has not been tested for API's outside of thenmap.net's API."
+    )
+    c <- readline(prompt = "Continue (Y/N)?")
+    if (all((c != "Y"), (c != "y"))) {
+      stop("Attempt to fetch data from non-standard API stopped.")
+    } else {
+      print(paste("All right, continuing with", url))
+    }
+  }
+  
+  # treating date as date format for later use
   date <- as.Date(date_str)
-  notes <- "" # pass a note if the date is missing for some reason
   
-  # constructs a url to fetch data from 
+  # pass a note if the date is missing for some reason
+  notes <- "" 
+  
+  # function to construct a url to fetch data from 
   urler <- function(url, date, contains_date) {
     if (contains_date == TRUE) {
-      url <- paste0(url, date)
+      # making sure the url is constructed properly depending on if the url end with a slash
+      if (substr(url, nchar(url), nchar(url)) == "/") {
+        url <- paste0(url,date)
+      } else {
+        url <- paste0(url, "/", date)
+      }
     }
     return(url)
   }
   
+  # calling fo he url to be constructed
   url <- urler(url = url, date = date, contains_date = contains_date)
   
   # function to fetch data and convert to a simple features object
@@ -43,7 +69,7 @@ fetch <- function(date_str = as.character(Sys.Date()), url = "http://api.thenmap
   # actually fetching the data
   fetched_data <- fetch_data(url)
   
-  # a nearly empty object should trigger a search for the next available date
+  # a nearly empty fetched object should trigger a search for the next available date and add a note
   cond <- function(fd) {
     utils::object.size(fd) < 100
   }
@@ -72,7 +98,7 @@ fetch <- function(date_str = as.character(Sys.Date()), url = "http://api.thenmap
   }
   
   # returning a list with the current geojson file and a note
-  # might make map_geojson a list of every fetched piece of data to preserve and speed up old queries
-  # (although that will start using too much space - perhaps the last 20 or something)
+  # might make data a list of every fetched piece of data to preserve and speed up old queries
+  # (although that will start using too much space after a few queries - perhaps the last 20 or something)
   return(list("data" = fetched_data, "notes" = paste0("Data fetched from: ", url, ". ", notes)))
 }
